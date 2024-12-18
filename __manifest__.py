c90ce34be7c6cdf553d63ccfa8076a319a16d690
al-varo <52884208+al-varo@users.noreply.github.com>
2024-12-19 03:15:01 +0700

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Fixed Discount on Invoice Lines',
+    'version': '1.0',
+    'summary': 'Add fixed discount field on invoice lines',
+    'author': 'Your Name',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
